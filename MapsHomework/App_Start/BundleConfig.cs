5c4ed916359537ffeda70867b25b53c0221dd215
Alexander Melara <lexmelara@gmail.com>
2015-09-18 22:17:35 -0600

    Subida de proyecto

---
+++ MapsHomework/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MapsHomework.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/contenido/js").Include(
+               "~/js/bootstrap.min.js",
+               "~/js/jquery-1.11.3.min.js"));
+
+            bundles.Add(new StyleBundle("~/contenido/css").Include(
+                "~/css/bootstrap.min.css",
+                "~/css/style.css",
+                "~/css/font-awesome.min.css"
+                ));
+        }
+    }
+}
